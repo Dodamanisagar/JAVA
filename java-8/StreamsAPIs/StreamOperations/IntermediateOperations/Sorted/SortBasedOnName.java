2e942b79de027fef529a08944c5fef3378b904af
Sagar Dodamani <99800998+Dodamanisagar@users.noreply.github.com>
2023-02-11 16:05:24 +0530

    Delete java-8/StreamsAPIs directory

--- java-8/StreamsAPIs/StreamOperations/IntermediateOperations/Sorted/SortBasedOnName.java
+++
@@ -1,9 +0,0 @@
-package StreamsAPIs.StreamOperations.IntermediateOperations.Sorted;
-import java.util.stream.*;
-public class SortBasedOnName {
-    public static void main(String[] args) {
-        Stream vegStream=Stream.of("tomato","Green Chilli","Potot0","POTOTO","BEetRoot");
-        Stream sortedStream=vegStream.sorted();
-        sortedStream.forEach(name->System.out.println(name));
-    }
-}
